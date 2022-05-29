13650a07d9b70d0515e1c8741f5f0b50a66afad1
Lawrence-Culley <lpc009@student.usc.edu.au>
2022-05-29 18:41:42 +1000

    Quick update
    -Makes the player respawn if they hit a drone

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -7,10 +7,12 @@ public class Respawn : MonoBehaviour
     //spawnPosition is the position that the player spawns at whenever they respawn
     public Vector3 spawnPosition = new Vector3(0f,0f,0f);
 
-    //checkpointTag is set to whatever the project uses to tag checkpoints
+    /*checkpointTag is set to whatever the project uses to tag checkpoints
+    enemyTag is used for when the player interacts with an enemy*/
     public string checkpointTag;
+    public string enemyTag;
 
-    //During each update, if the F key is pressed, call toRespawn
+    //During each update, if the F key is pressed, calls the toRespawn function
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
@@ -19,13 +21,18 @@ public class Respawn : MonoBehaviour
         }
     }
 
-    /*When colliding with an object, checks to see if the tag is the same as the CheckpointTag
-    if it is the same, sets the spawnPosition to the location of the object it collided with*/
+    /*When colliding with an object, checks to see what the tag is
+    if it is the checkpointTag, sets the spawnPosition to the location of the object it collided with
+    if it is enemyTag, calls the toRespawn function*/
     void OnTriggerEnter(Collider collision)
     {
         if(collision.tag == checkpointTag)
         {
             spawnPosition = collision.transform.position;
+        }
+        if(collision.tag == enemyTag)
+        {
+            toRespawn();
         }
     }
 

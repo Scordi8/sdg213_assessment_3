e5829efad4820bb88053290f15191f60f798c2e1
Scordi <s_d161@student.usc.edu.au>
2022-06-07 16:35:33 +1000

    Added Camera oscillation

--- Assets/Scripts/CameraMotion.cs
+++ Assets/Scripts/CameraMotion.cs
@@ -25,11 +25,16 @@ public class CameraMotion : MonoBehaviour
     public GameObject yaw;
     public GameObject pitch;
 
+    private int turndir = 1;
+    private float turnspeedramp = 0;
+
+    private Vector3[] bounds;
 
     // Start is called before the first frame update
     void Start()
     {
         
+        bounds = new Vector3[] {new Vector3(0, -_turnAngle, 0), new Vector3(0, _turnAngle, 0) };
     }
 
     void trackto(Vector3 pos)
@@ -41,12 +46,41 @@ public class CameraMotion : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void setTracker(GameObject? target)
+    {
+        if (target != null)
+        {
+            track = target;
+            canTrack = true;
+        }
+        else
+        {
+            track = null;
+            canTrack = false;
+        }
+    }
+
+
+    // Fixed update responsible for checking whether to track an object or to oscillate
+    void FixedUpdate()
     {
         if (canTrack && track != null)
         {
             trackto(track.transform.position);
+        }
+        else
+        {
+            if (!_static)
+            {
+                float angle = yaw.transform.eulerAngles.y;
+                if (angle > 180) { angle = angle - 360; }
+
+                if (angle > bounds[1].y) { turndir = -1; turnspeedramp = 0; }
+                if (angle < bounds[0].y) { turndir = 1; turnspeedramp = 0; }
+                yaw.transform.eulerAngles = new Vector3(0, Mathf.Clamp(angle, -_turnAngle, _turnAngle), 0);
+
+                yaw.transform.eulerAngles = Vector3.Lerp(yaw.transform.eulerAngles, new Vector3(yaw.transform.eulerAngles.x, yaw.transform.eulerAngles.y + (_turnspeed*turndir), yaw.transform.eulerAngles.z), 0.5f);
+            }
         }
     }
 }
